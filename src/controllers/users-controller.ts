5886cf56052b80880bb07d950e664eaf18c91c2d
Antonyj26 <antonyjosu26@gmail.com>
2025-08-20 10:04:03 -0300

    Validating user data

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,7 +1,26 @@
 import { Request, Response } from "express";
+import { z } from "zod";
+import { prisma } from "@/database/prisma";
+import { AppError } from "@/utils/AppError";
+import { UserRole } from "@prisma/client";
 
 class UserController {
   async create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      name: z.string().trim().min(2, { message: "Nome é obrigatório" }),
+      email: z
+        .string()
+        .trim()
+        .email({ message: "Email inválido" })
+        .toLowerCase(),
+      password: z.string().min(6, "A senha deve ter pelo menos 6 dígitos"),
+      role: z
+        .enum([UserRole.employee, UserRole.manager])
+        .default(UserRole.employee),
+    });
+
+    const { name, email, password, role } = bodySchema.parse(request.body);
+
     return response.json({ message: "ok" });
   }
 }
