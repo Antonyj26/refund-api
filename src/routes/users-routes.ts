9e2b0c4029f416ed411013f5304c52595f154f25
Antonyj26 <antonyjosu26@gmail.com>
2025-08-20 01:27:44 -0300

    Route and Controller to create user

---
+++ src/routes/users-routes.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import { UserController } from "@/controllers/users-controller";
+
+const usersRoutes = Router();
+const userController = new UserController();
+
+usersRoutes.post("/", userController.create);
+
+export { usersRoutes };
